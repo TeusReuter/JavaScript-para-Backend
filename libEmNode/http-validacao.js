acb9ec661e9900b6a5cdbaf2b5707e1e323529de
Matheus Alexsander Reuter <86987427+TeusReuter@users.noreply.github.com>
2022-04-08 17:12:12 -0300

    Add files via upload

---
+++ libEmNode/http-validacao.js
@@ -0,0 +1,40 @@
+const fetch = (...args) => import('node-fetch').then(({
+  default: fetch
+}) => fetch(...args));
+
+function handleErros(erro) {
+  throw new Error(erro.message)
+}
+
+async function checaStatus(arrayURLs) {
+  try {
+    const arrayStatus = await Promise
+      .all(arrayURLs
+        .map(async url => {
+          const res = await fetch(url)
+          return res.status
+        }))
+    return arrayStatus
+  } catch(erro) {
+    handleErros(erro)
+  }
+}
+
+function geraArrayDeURLs(arrayLinks) {
+  return arrayLinks
+    .map(objetoLink => Object
+      .values(objetoLink).join())
+}
+
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeURLs(arrayLinks)
+  const statusLinks = await checaStatus(links)
+
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: statusLinks[indice]
+  }))
+  return resultados
+}
+
+module.exports = validaURLs
---
+++ libEmNode/index.js
@@ -0,0 +1,34 @@
+const chalk = require('chalk')
+const fs = require('fs')
+
+
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+  const arrayResultados = []
+  let temp;
+
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({
+      [temp[1]]: temp[2]
+    })
+  }
+  return arrayResultados.length === 0 ? 'não há links' : arrayResultados
+}
+
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
+}
+
+
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8'
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks((texto))
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+module.exports = pegaArquivo
